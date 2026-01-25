a30ebb904bd91186939cb66a9a6d8db318364a77
Pratyush Ranjan Prasad <149002049+Pratyush-48@users.noreply.github.com>
2026-01-25 11:42:27 +0530

    Time: 2 ms (71.7%), Space: 8.9 MB (96.13%) - LeetHub

--- 3821-find-nth-smallest-integer-with-k-one-bits/3821-find-nth-smallest-integer-with-k-one-bits.cpp
+++ 3821-find-nth-smallest-integer-with-k-one-bits/3821-find-nth-smallest-integer-with-k-one-bits.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    long long combi[60][60];
+    long long combi[51][51];
     void pre(){
-        for(int i=0;i<60;i++){
+        for(int i=0;i<51;i++){
             combi[i][0] = 1;
             for(int j=1;j<=i;j++){
                 combi[i][j] = combi[i-1][j-1] + combi[i-1][j];
@@ -12,7 +12,7 @@ public:
     long long nthSmallest(long long n, int k) {
         long long result = 0;
         pre();
-        for(int i=55;i>=0;i--){
+        for(int i=50;i>=0;i--){
             long long v = (i>=k) ? combi[i][k] : 0;
             if(n>v){
                 result |= (1LL << i);
