eaafd559f19ea93dd0cb24bbd135e7acff0fd0b9
Pratyush Ranjan Prasad <149002049+Pratyush-48@users.noreply.github.com>
2026-02-16 16:51:26 +0530

    Time: 4 ms (29.28%), Space: 8.3 MB (15.72%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -2,20 +2,10 @@ class Solution {
 public:
     int reverseBits(int n) {
         long long result = 0;
-        vector<int>bits;
-        while(n){
-            int m = n%2;
-            n/= 2;
-            bits.push_back(m);
-        }
-        //reverse(bits.begin(),bits.end());
-        for(int i=0;i<bits.size();i++){
+        for(int i=0;i<32;i++){
             result = result<<1;
-            result += bits[i];
-        }
-        int d = 32-bits.size();
-        while(d--){
-            result <<= 1;
+            if(n&1) result++;
+            n >>= 1;
         }
         return result;
     }
