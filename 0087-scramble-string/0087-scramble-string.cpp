3ca23df79d49e399979d3feab76ca4e4330e08a8
Pratyush Ranjan Prasad <149002049+Pratyush-48@users.noreply.github.com>
2026-04-29 09:56:06 +0530

    Time: 299 ms (5.05%), Space: 67.8 MB (5.08%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    unordered_map<string,bool>dp;
-    bool recc(string s1,string s2){
+    bool recc(string s1,string s2,unordered_map<string,bool>&dp){
         if(s1==s2) return true;
         if(s1.length() != s2.length())
             return false;
@@ -11,15 +10,16 @@ public:
         }
         int n = s1.length();
         for(int i=1;i<n;i++){
-            bool swapped = recc(s1.substr(i,n-i),s2.substr(0,n-i)) && recc(s1.substr(0,i),s2.substr(n-i,i));
+            bool swapped = recc(s1.substr(i,n-i),s2.substr(0,n-i),dp) && recc(s1.substr(0,i),s2.substr(n-i,i),dp);
             if(swapped) return true;
-            bool not_swapped = recc(s1.substr(i,n-i),s2.substr(i,n-i)) && recc(s1.substr(0,i),s2.substr(0,i));
+            bool not_swapped = recc(s1.substr(i,n-i),s2.substr(i,n-i),dp) && recc(s1.substr(0,i),s2.substr(0,i),dp);
             if(not_swapped) return true;
         }
         return dp[key]=false;
     }
     bool isScramble(string s1, string s2) {
         // basicaly we need to check by swapping or not swapping
-        return recc(s1,s2);
+        unordered_map<string,bool>dp;
+        return recc(s1,s2,dp);
     }
 };
