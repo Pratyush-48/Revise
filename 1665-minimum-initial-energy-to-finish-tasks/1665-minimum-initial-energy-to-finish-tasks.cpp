154566ac2f43a55dda947bce3ffdf9d7eee0bf5e
Pratyush Ranjan Prasad <149002049+Pratyush-48@users.noreply.github.com>
2026-05-12 09:04:23 +0530

    Time: 39 ms (60.68%), Space: 97.3 MB (85.92%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
-    bool bs(vector<vector<int>>&tasks,int mid,int n){
+    int minimumEffort(vector<vector<int>>& tasks) {
+        int n = tasks.size();
+        sort(tasks.begin(), tasks.end(), [](const vector<int>& a, const vector<int>& b) {
+            return (a[1] - a[0]) > (b[1] - b[0]);
+        });
         long start = tasks[0][1];
         long curr = start;
         for(int i=0;i<n;i++){
@@ -14,26 +18,6 @@ public:
                 curr -= tasks[i][0];
             }
         }
-        return start<=mid;
-    }
-    int minimumEffort(vector<vector<int>>& tasks) {
-        int n = tasks.size();
-        sort(tasks.begin(), tasks.end(), [](const vector<int>& a, const vector<int>& b) {
-            return (a[1] - a[0]) > (b[1] - b[0]);
-        });
-        int right = 1e9;
-        int left = 1;
-        int result = -1;
-        while(left<=right){
-            int mid = (left+right)/2;
-            if(bs(tasks,mid,n)){
-                result = mid;
-                right = mid-1;
-            }
-            else{
-                left = mid+1;
-            }
-        }
-        return result;
+        return start;
     }
 };
