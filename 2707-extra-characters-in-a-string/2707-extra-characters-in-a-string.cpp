c444c055732ac5459fc0b895d55c468a40dc1c98
Pratyush Ranjan Prasad <149002049+Pratyush-48@users.noreply.github.com>
2026-06-02 16:26:26 +0530

    Time: 169 ms (15.45%), Space: 99.5 MB (28.42%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -8,7 +8,7 @@ public:
         if(dp[idx]!=-1) return dp[idx];
         if(st.find(s.substr(idx))!=st.end()) return dp[idx]=0;
         int result = INT_MAX;
-        for(int l = 1;l<=n;l++){
+        for(int l = 1;idx+l<=n;l++){
             string temp = s.substr(idx,l);
             if(st.find(temp)!=st.end()){
                 result = min(result,recc(idx+l,s));
